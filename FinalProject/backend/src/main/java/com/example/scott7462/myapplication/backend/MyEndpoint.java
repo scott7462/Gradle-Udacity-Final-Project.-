f0f238aaa58d4e5f5e240ef0d37d2e05a5797a2e
Pedro Scott <scott7462@gmail.com>
2016-04-24 22:07:00 -0500

    Backend
    Type: Refactor.
    Body: Add the google end point for manager the tests service and send
    the joke by the java library.

---
+++ FinalProject/backend/src/main/java/com/example/scott7462/myapplication/backend/MyEndpoint.java
@@ -0,0 +1,38 @@
+/*
+   For step-by-step instructions on connecting your Android application to this backend module,
+   see "App Engine Java Endpoints Module" template documentation at
+   https://github.com/GoogleCloudPlatform/gradle-appengine-templates/tree/master/HelloEndpoints
+*/
+
+package com.example.scott7462.myapplication.backend;
+
+import com.example.Jokes;
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiNamespace;
+
+import javax.inject.Named;
+
+/**
+ * An endpoint class we are exposing
+ */
+@Api(
+        name = "myApi",
+        version = "v1",
+        namespace = @ApiNamespace(
+                ownerDomain = "backend.myapplication.scott7462.example.com",
+                ownerName = "backend.myapplication.scott7462.example.com",
+                packagePath = ""
+        )
+)
+public class MyEndpoint {
+
+    /**
+     * A simple endpoint method that takes a name and says Hi back
+     */
+    @ApiMethod(name = "sayAJoke")
+    public Jokes sayAJoke() {
+        return new Jokes();
+    }
+
+}
