e7212b5c7a9ee4c22502a6b6b74685f69df60863
Pedro Scott <scott7462@gmail.com>
2016-04-24 22:10:27 -0500

    Async task
    Type: Refactor.
    Body: Add the async task to manager the connection of the service to
    get jokes.

---
+++ FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/rest/EndpointsAsyncTask.java
@@ -0,0 +1,56 @@
+package com.udacity.gradle.builditbigger.rest;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+
+import com.example.scott7462.myapplication.backend.myApi.MyApi;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import com.proggmail.jud.jokeandroid.JokeActivity;
+
+import java.io.IOException;
+
+/**
+ * Created by scott7462 on 4/24/16.
+ */
+public class EndpointsAsyncTask extends AsyncTask<Context, Void, String> {
+    private static MyApi myApiService = null;
+    private Context context;
+
+    @Override
+    protected String doInBackground(Context... params) {
+        if (myApiService == null) {  // Only do this once
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                    new AndroidJsonFactory(), null)
+                    // options for running against local devappserver
+                    // - 10.0.2.2 is localhost's IP address in Android emulator
+                    // - turn off compression when running against local devappserver
+                    .setRootUrl("http://10.0.2.2:8080/")
+                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                        @Override
+                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                            abstractGoogleClientRequest.setDisableGZipContent(true);
+                        }
+                    });
+            myApiService = builder.build();
+        }
+        context = params[0];
+
+        try {
+            return myApiService.sayAJoke().execute().getJoke();
+        } catch (IOException e) {
+            return e.getMessage();
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        Intent intent = new Intent(context, JokeActivity.class);
+        intent.putExtra(JokeActivity.JOKE_TEXT, result);
+        context.startActivity(intent);
+//        Toast.makeText(context, result, Toast.LENGTH_LONG).show();
+    }
+}
