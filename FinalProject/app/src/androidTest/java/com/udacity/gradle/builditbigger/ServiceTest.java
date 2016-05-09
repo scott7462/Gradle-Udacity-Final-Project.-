7acc4085d19a4d843a03fb59be37f41247868b68
Pedro Scott <scott7462@gmail.com>
2016-05-09 14:38:29 -0500

    Licence
    Type: Refactor.
    Body: Add the license of the java files.

--- FinalProject/app/src/androidTest/java/com/udacity/gradle/builditbigger/ServiceTest.java
+++ FinalProject/app/src/androidTest/java/com/udacity/gradle/builditbigger/ServiceTest.java
@@ -10,11 +10,23 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 /**
- * Created by scott7462 on 4/25/16.
+ * Copyright (C) 2015 The Android Open Source Project
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 public class ServiceTest extends AndroidTestCase implements ResultJokeListener {
     CountDownLatch signal;
-    String joke ="";
+    String joke = "";
 
     protected void setUp() throws Exception {
         super.setUp();
@@ -22,11 +34,10 @@ public class ServiceTest extends AndroidTestCase implements ResultJokeListener {
     }
 
     @UiThreadTest
-    public void testDownload() throws InterruptedException
-    {
+    public void testDownload() throws InterruptedException {
         new EndpointsAsyncTask(this).execute();
         signal.await(30, TimeUnit.SECONDS);
-        assertTrue("Service completed, the result are here!", joke !=null);
+        assertTrue("Service completed, the result are here!", joke != null);
     }
 
     @Override
